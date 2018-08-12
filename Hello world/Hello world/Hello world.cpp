aa64fe039a6655fc9f55627c14b5f461bd62dbf1
Failedkey <keyfailed@gmail.com>
2018-08-12 10:51:41 -0700

    Basic hello world

---
+++ Hello world/Hello world/Hello world.cpp
@@ -0,0 +1,14 @@
+// Hello world.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <Windows.h>
+
+//Simple hello world
+int main(void)
+{
+	printf("Hello world");
+	system("Pause");
+    return 0;
+}
+
